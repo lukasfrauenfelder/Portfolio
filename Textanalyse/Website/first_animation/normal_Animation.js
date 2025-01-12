db1fb7ea2c9d6fbc6c704a394018b62ea20c3148
lukasfrauenfelder <168639540+lukasfrauenfelder@users.noreply.github.com>
2025-01-12 12:12:13 +0100

    Update normal_Animation.js

--- Textanalyse/Website/first_animation/normal_Animation.js
+++ Textanalyse/Website/first_animation/normal_Animation.js
@@ -176,7 +176,7 @@ function draw() {
 }
 function navigateToNewPage() {
   console.log("gugus?");
-  const baseURL = "/Textanalyse/Website/big_animation/index.html"; // Update with your new page's path
+  const baseURL = "big_animation/index.html"; // Update with your new page's path
   const query = selectedAlbums.map(encodeURIComponent).join(","); // Encode album titles
   window.location.href = `${baseURL}?albums=${query}`;
 }
